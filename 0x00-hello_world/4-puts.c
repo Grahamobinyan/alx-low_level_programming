e6ec60f0d939975ab0540d6a4d36c69b810efcbd
grahamobinyan <grahamobinyan@gmail.com>
2022-04-07 19:00:38 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <studio.h>
+
 /**
- *main - entry point
+ * main - Entry point
  *
- *Return: 0 after printing the function
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+puts("/"programming is like building a multilingual puzzle");
+return (0)
 }
