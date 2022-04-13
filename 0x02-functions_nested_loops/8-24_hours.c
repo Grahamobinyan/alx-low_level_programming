f3ea1e9ca8fd953f906195b4a7dfbf0a51bb64b2
grahamobinyan <grahamobinyan@gmail.com>
2022-04-13 13:47:44 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,7 @@
 /**
  * jack_bauer -prints every minute of the day
  *
- * Return: always 0.
+ * Return: Always 0.
  */
 
 void jack_bauer(void)
@@ -19,7 +19,7 @@ for (b = 0; b <= 59; b++)
 _putchar (a / 10 + '0');
 _putchar (a % 10 + '0');
 _putchar (':');
-_putchar (b / 10 + 'o');
+_putchar (b / 10 + '0');
 _putchar (b % 10 + '0');
 _putchar ('\n');
 
